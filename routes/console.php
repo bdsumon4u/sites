3f450388da56c39af5ce84127fabb86470fcc75f
Sumon Ahmed <bdsumon4u@gmail.com>
2025-02-14 19:30:53 +0600

    fix: increase timeout for queue:work command to enhance job processing

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('queue:work --tries=3 --delay=60 --stop-when-empty')
+Schedule::command('queue:work --tries=3 --delay=60 --timeout=600 --stop-when-empty')
     ->everyMinute()
     ->runInBackground()
     ->withoutOverlapping()
